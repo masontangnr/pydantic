7102c7e90a04ce5d822661fca8ba354d4eba7617
masontangnr <masontang0@gmail.com>
2025-09-30 14:43:47 -0500

    Update requirements.txt with a comprehensive list of dependencies

---
+++ README.md
@@ -0,0 +1,9 @@
+## Description
+This project is to learn more about pydantic and look through the documentation
+
+## How to start this project
+1. source .venv/Scripts/activate
+2. pip install -r requirements.txt
+3. python __fileName.py
+
+
---
+++ hello_world.py
@@ -0,0 +1,25 @@
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIChatModel
+from pydantic_ai.providers.openrouter import OpenRouterProvider
+import os
+from dotenv import load_dotenv
+
+# Load API key from .env
+load_dotenv()
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+# Define your model using OpenRouter's OpenAI-compatible API
+model = OpenAIChatModel(
+    'x-ai/grok-4-fast:free',
+    provider=OpenRouterProvider(api_key=api_key),
+)
+
+# Define your agent
+agent = Agent(
+    model=model,
+    instructions='Be concise, reply with one sentence.',
+)
+
+# Run query
+result = agent.run_sync('Where does "hello world" come from?')
+print(result.output)
